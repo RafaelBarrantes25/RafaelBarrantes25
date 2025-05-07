85a14ddd2d603f78cc14c91b0138f65df75bbc9d
RafaelBarrantes25 <jorabaqu2580@gmail.com>
2025-05-06 21:29:43 -0600

    Commit

--- prueba4.py
+++ prueba4.py
@@ -78,30 +78,30 @@ def imprimir_valores():
     return imprimir_valores_aux(generar_comunidades(),"")
 
 
+
 def imprimir_valores_aux(lista, respuesta):
     #Imprime las comunidades y sus valores
     if contar_elementos_cola(lista) == 0:
         print(respuesta)
-        return verificar_valores(lista,[])
     else:
         return imprimir_valores_aux(lista[1:],respuesta+str(lista[0][0])+": "+"Acervo: "+str(lista[0][1])+" "+"Autonomía: "+str(lista[0][2])+"\n")
 
 
 
 def verificar_valores(lista1,lista2):
-    if len(lista1) == 0:
+    if lista1 == []:
         return restar_valores(lista2)
     elif lista1[0][1] <= 0 or lista1[0][2] <= 0:
         return finalizar(lista1)
     else:
-        return verificar_valores(lista1[1:],lista2+[lista1[0]])
+        return verificar_valores(lista1[1:],[lista2]+[lista1[0]])
 
 
 def finalizar(lista):
     return "Fin del juego"
 
 def restar_valores(lista):
-    lista2 = lista[random.randint(0,len(lista))][random.randint(1,2)]-1
+    lista2 = lista[random.randint(0,len(lista)-1)][random.randint(1,2)]-1
     print(imprimir_valores_aux(lista2,[]))
 
 imprimir_valores()
---
+++ prueba6.py
@@ -0,0 +1,4 @@
+lista = [["Sapo",1,2],["sepo",4,6],["srtwr",7,2]]
+
+if lista[0][1] == 1:
+    print("Sapo")
